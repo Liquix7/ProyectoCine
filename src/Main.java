a24ec81763ca7446daf97c442bb24ac1c5bdec6f
Liqui <liquix123@gmail.com>
2024-02-16 13:21:44 +0100

    (feat): Creado el main donde ejecutaremos el programa,Creamos los obejtos que vamos a usar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        Cine cine = new Cine("Cine XYZ", 100);
+
+        Pelicula pelicula1 = new Pelicula("Avengers", 120);
+        Sala sala1 = new Sala(1, pelicula1, 5, 5);
+        cine.agregarSala(sala1);
+
+        Pelicula pelicula2 = new Pelicula("Minions", 90);
+        Sala sala2 = new Sala(2, pelicula2, 6, 6);
+        cine.agregarSala(sala2);
+
     }
 }
